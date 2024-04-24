458166396ff1ded091637808884f90e6befd9283
Ndeye <ndeyet2004@gmail.com>
2024-04-24 16:54:57 -0700

    finish all the problems

--- problems/02-pseudocode.js
+++ problems/02-pseudocode.js
@@ -13,14 +13,55 @@ Given a number, write a function called within5Of100 to return true if the given
 number is within 5 of 100.
 */
 
-// console.log(within5Of100(94));  //=> false
-// console.log(within5Of100(95));  //=> true
-// console.log(within5Of100(100)); //=> true
-// console.log(within5Of100(105)); //=> true
-// console.log(within5Of100(106)); //=> false
+// goal: create a function that check if given number is in between 5 and 100 including 5 and 100 and return true else false
+// input: number
+// output: boolean
+
+// create a function within5Of100 with one parameter.
+function within5Of100(num){
+//use if statement to check if the number is greater than or equal to 5 and less than or equal to 100.
+// return boolean
+    if(num >= 5 && num <= 100){
+        return true;
+    }
+    return false;
+
+}
+
+// test number less 5.
+console.log(within5Of100(4)); //=> false
+// test number betwen 5 and 100.
+console.log(within5Of100(94));  //=> true
+console.log(within5Of100(95));  //=> true
+
+// test 5 and 100
+console.log(within5Of100(100)); //=> true
+console.log(within5Of100(5)); //=> true
+
+//test numbers greater than 100.
+console.log(within5Of100(105)); //=> false
+ console.log(within5Of100(106)); //=> false
 
 // CHALLENGE: Can you design a function that would return true if a given number
 // is within X of another given number Y?
+
+console.log("------------------------------------------------------------------------------------");
+
+//create a function checkNumbetweenXandY that take three parameter x and y and a number to be check.
+function checkNumbetweenXandY(start, end, num){
+    // use condition to search from to start end for the number
+    if(num >= start && num <= end){
+    // return true else return false
+        return true;
+    }
+    return false;
+}
+
+//test
+console.log(checkNumbetweenXandY(5,100, 94)); // => true
+console.log(checkNumbetweenXandY(1, 1000, 1001)); // => false
+console.log(checkNumbetweenXandY(5, 100, 5)); // => true
+console.log(checkNumbetweenXandY(5,100, 100)); // => true
 
 
 /******************** DO NOT MODIFY ANY CODE BELOW THIS LINE *****************/
--- problems/03-commenting-code.js
+++ problems/03-commenting-code.js
@@ -5,13 +5,13 @@ Replace variables using the example input in code comments. Then fix the bug
 in the code so that the example input matches the expected output.
 */
 
-function charactersNotQuiteAtTheEdge(str) {
-  let char1 = str[1];
-  let char2 = str[str.length - 1];
-  return char1 + char2;
+function charactersNotQuiteAtTheEdge(str) { // str = "fringe"
+  let char1 = str[1];                       // char1 = "r"
+  let char2 = str[str.length - 2];          // char2 = "g"
+  return char1 + char2;                     // "rg"
 }
 
-// console.log(charactersNotQuiteAtTheEdge('fringe')); // expected output: 'rg'
+console.log(charactersNotQuiteAtTheEdge('fringe')); // expected output: 'rg'
 
 
 /******************** DO NOT MODIFY ANY CODE BELOW THIS LINE *****************/
